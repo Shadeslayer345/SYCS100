06f8d40148ab91d8a991adbe11b966fb036a03ce
Barry Cornell Harris Jr. <bharrisaplus@gmail.com>
2013-11-17 18:48:20 -0500

    Re-added Barry's binary search function!

--- projects/bsearch/r3-Bo0tbsearch.py
+++ projects/bsearch/r3-Bo0tbsearch.py
@@ -74,3 +74,33 @@ def bsearch(wlist, item):
             else:
                 beginning = middle
     return -1
+    
+  ''' Barry's Function (Lines 78 - 105 '''
+  ''' Only run the Bsearch function '''
+  ''' Recursive Function To Search The Sorted List (Barry) '''
+def search(inputlist, searchValue, startSearchParameter, endSearchParameter):
+		print len(inputlist)
+		middleValue = ((startSearchParameter+endSearchParameter)/2)
+		if (startSearchParameter <= endSearchParameter):
+			if searchValue == inputlist[middleValue]:
+				return middleValue
+			else:	
+				if searchValue < inputlist[middleValue]:
+					endSearchParameter = middleValue - 1
+					return search(inputlist, searchValue, startSearchParameter, endSearchParameter)
+				else:
+					startSearchParameter = middleValue + 1
+					return search(inputlist, searchValue, startSearchParameter, endSearchParameter)
+		else:
+			return -1
+			
+''' Final Main Function Search The List (Barry) '''
+def binarySearch(list, searchValue):
+	print "\nBinary Searching........."
+	startParameter = 0
+	endParameter = len(list)-1
+	message = search(list, searchValue, startParameter, endParameter)
+	if message == -1:
+		print "Search Value not found.... %d does not appear to be in the given list" %searchValue
+	else:
+		print "\nSearch value %d found at position %d" % (searchValue, message)
